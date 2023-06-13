9df3a35979d8780f287a1315328894a1b2fdaff8
akatz6 <aaronk488@gmail.com>
2023-06-12 17:20:28 -0700

    demo of useEffect, fetch, and axios

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import RickAndMorty from "./RickAndMorty";
+
+function App() {
+  const [holiday, setHoliday] = useState([]);
+  const [rickAndMorty, setRickAndMorty] = useState([]);
+
+  useEffect(() => {
+    // using axios
+    try {
+      const getHolidays = async () => {
+        const response = await axios.get(
+          "https://date.nager.at/api/v2/publicholidays/2023/US"
+        );
+        const info = response.data;
+        setHoliday(info);
+      };
+    } catch (e) {
+      console.log(e);
+    }
+
+    const getRickAndMorty = async () => {
+      //using fetch
+      try {
+        const response = await fetch(
+          "https://rickandmortyapi.com/api/character?page=1"
+        );
+        const data = await response.json();
+        setRickAndMorty(data.results);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getHolidays();
+    getRickAndMorty();
+  }, []);
+
+  return (
+    <>
+      <div>
+        {holiday.map((element) => {
+          return <p key={element.date}> {element.name}</p>;
+        })}
+      </div>
+      <div>
+        {rickAndMorty.map((element) => {
+          return (
+            <RickAndMorty
+              key={element.id}
+              name={element.name}
+              img={element.image}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/RickAndMorty.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+export default function RickAndMorty({name, img}) {
+    console.log(name)
+    console.log(img)
+  return (
+    <></>
+  )
+}
